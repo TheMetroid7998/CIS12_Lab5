d18da10746bf05cf81c048a17116a6a5bdcb1779
Conrad Salazar <themetroid7998@gmail.com>
2024-10-02 22:19:14 -0700

    Completion of Lab 5 Extra.

--- CIS12_Lab5.py
+++ CIS12_Lab5.py
@@ -1,5 +1,3 @@
-#import math
-
 """
 def is_valid_range(part):
     if 0 <= part <= 255:
@@ -32,24 +30,19 @@ def is_valid(part):
 
 def is_valid_ip(ip):
     """Splits the ip address into a list of the 4 numbers and checks each item in the list."""
+    ip = str(ip)
     split_ip = ip.split('.')
 
     if len(split_ip) != 4:
         print("This IP Address is not valid: IP Address is not the correct length.")
-        return
+        return False
 
     for item in split_ip:
         if not is_valid(item):
             print("This IP Address is not valid: IP Address values are invalid.")
-            return
+            return False
     print("This IP Address is valid.")
-    """
-    Original that I came up with, not sure how to integrate the checks with this version.
-    n = 0
-    while n < len(split_ip):
-        is_valid(split_ip[n])
-        n = n + 1
-    """
+    return True
 
 def decimal_to_binary(n):
     """Performs basic checks before passing the decimal number to a helper function."""
@@ -66,14 +59,51 @@ def decimal_to_binary_helper(n):
     else:
         return decimal_to_binary_helper(n // 2) + str(n % 2)
 
-def binary_to_decimal(binary_str):
+def binary_to_decimal(bin_str):
     """Converts binary to decimal values. Validates the number before beginning the conversion process."""
-    binary_str = str(binary_str)
-    if not all(char in '01' for char in str(binary_str)):
+    bin_str = str(bin_str)
+    if not all(char in '01' for char in str(bin_str)):
         return "This number is not a valid binary number."
-    decimal_val = 0
-    length = len(binary_str)
+    dec_val = 0
+    length = len(bin_str)
     for i in range(length):
-        decimal_val += int(binary_str[i]) * (2 ** (length - 1 - i))
-    return decimal_val
+        dec_val += int(bin_str[i]) * (2 ** (length - 1 - i))
+    return dec_val
 
+def ip_to_binary(ip):
+    if is_valid_ip(ip):
+        # print("IP Address is valid and ready for conversion.")
+        pass
+    else:
+        return "Invalid IP Addresses cannot be converted."
+    split_ip = ip.split('.')
+    bin_ip = []
+    for item in split_ip:
+        bin_part = decimal_to_binary(int(item))
+        bin_ip.append(bin_part.zfill(8))
+#   Original Code (Before asking ChatGPT): bin_ip += [''.join(decimal_to_binary(item)) for item in split_ip]
+    return '.'.join(bin_ip)
+
+def binary_to_ip(bin_ip):
+    split_ip = bin_ip.split('.')
+    dec_ip = []
+    for item in split_ip:
+        dec_part = binary_to_decimal(str(item))
+        dec_ip.append(dec_part)
+    dec_full = '.'.join(map(str, dec_ip))
+    if is_valid_ip(dec_full):
+        return dec_full
+    else:
+        return "Invalid IP Addresses cannot be converted."
+
+def ip_convert(ip):
+    if all(char in '01.' for char in str(ip)):
+        return binary_to_ip(ip)
+    else:
+        return ip_to_binary(ip)
+
+print(ip_convert('192.168.1.1'))
+print(ip_convert('11000000.10101000.00000001.00000001'))
+
+#print(ip_to_binary('192.168.1.1'))
+#print(binary_to_ip('11000000.10101000.00000001.00000001'))
